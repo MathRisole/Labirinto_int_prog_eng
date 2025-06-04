2f738883b35d42b55322440830790f8127003168
Risole <matheus.rizziolli@usp.br>
2025-06-04 20:21:33 -0300

    Mais oeganizacao e formatacao

---
+++ Source/Gerador_labrinto.py
@@ -0,0 +1,6 @@
+#Importando as bibliotecas necessarias
+import pygame as pg   #Para criar janelas, desenhar objetos, etc
+import random as r    #Para gerar numeros aleatorios, embaralhar listas, etc
+import datetime       #Para registrar tempos
+import os             #Gerenciamento de arquivos/diretorios
+import pathlib        #Gerenciamento de arquivos/diretorios
