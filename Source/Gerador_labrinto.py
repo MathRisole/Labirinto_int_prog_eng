77ed7c9c722c7a0a73465bc39c4f360ba7b2997d
Rafael-OVB <rafaelvillasboas07@gmail.com>
2025-06-06 13:37:52 -0300

    Faz ai matheus

--- Source/Gerador_labrinto.py
+++ Source/Gerador_labrinto.py
@@ -14,42 +14,27 @@ import pathlib        #Gerenciamento de arquivos/diretorios
 #Caracteres usados:
 #Para o labirinto usarei "#" para paredes e "_" para espaços vazios.(sujeito a mudanças)
 
-def criar_esqueleto_labrinto(largura, altura):
-            #Criando uma matriz para o labirinto
-            labirinto = []
-            for i in range(altura):
-                linha = []
-                for j in range(largura):
-                    if i == 0 or i == altura - 1 or j == 0 or j == largura - 1:  
-                        linha.append("#")  # Paredes
-                    else:
-                        linha.append("_")  # Espaços vazios/ nao gerados . "." == espaço vazio gerado 
-                labirinto.append(linha)
-            return labirinto
+
+
+
 #Criando a classe do labirinto
+
 class Labirinto:
-    def __init__(self, largura, altura):
-        #atributos do labirinto:
-        self.largura = largura
-        self.altura = altura
-        #self.dificuldade = xxxx  # Pode ser adicionado depois
-        self.labirinto = criar_esqueleto_labrinto(largura, altura)
-        
-
-
-    #printar bonitinho
-    def printBonito(self):
-        return "\n".join("".join(linha) for linha in self.labirinto)
-
-
-
-
-    def gerar_direcao():
-        direcoes = ["cima", "baixo", "esquerda", "direita"]
-        return r.choice(direcoes)
+    def criar_esqueleto_labrinto(self, largura, altura):
+        #Criando uma matriz para o labirinto
+        self.matriz = []
+        for i in range(altura):
+            linha = []
+            for j in range(largura):
+                if i == 0 or i == altura - 1 or j == 0 or j == largura - 1:  
+                    linha.append("#")  # Paredes
+                else:
+                    linha.append(" ")  # Espaços vazios/ nao gerados . "." == espaço vazio gerado 
+            self.matriz.append(linha)
+        return self.matriz
     
 
-    def verificar_direcao(diereita, esquerda, baixo , cima): #passar os 4 pontos diretamente adjacentes ao ponto atual
+    def verificar_direcao(self, diereita, esquerda, baixo , cima): #passar os 4 pontos diretamente adjacentes ao ponto atual
         #verificar se ao redor do ponto só existem dois caminhos 
         livres = 0
         if diereita == ".":
@@ -61,30 +46,59 @@ class Labirinto:
         if cima == ".":
             livres += 1
         return livres <= 2
-    def gerar_caminho_aleatorio(labirinto, largura, altura):
-        caminho =[]
+    
+    def __init__(self, largura, altura):
+        #atributos do labirinto:
+        self.largura = largura
+        self.altura = altura
+        #self.dificuldade = xxxx  # Pode ser adicionado depois
+        self.matriz = self.criar_esqueleto_labrinto(largura, altura)
+
+    def gerar_direcao(self):
+        direcoes = ["cima", "baixo", "esquerda", "direita"]
+        return r.choice(direcoes)
+
+
+    def gerar_caminho_aleatorio(self, largura, altura):
         #começar em ponto aleatorio
-        x = r.randint(1, largura - 1)
-        y = r.randint(1, altura - 1)
-        labirinto[y][x] = "E"
+        x = 2
+        y = altura//2
+        self.matriz[y][0] = "E"
+        self.matriz[y][1] = "."
         tamanho = r.randint(1, (largura * altura) // 6)
-        for i in range(tamanho):
-            direcao = gerar_direcao()#verificar se a direcao é valida. não é uma parede e nem ligada a dois caminhos
-            if direcao == "cima" and y > 1 and verificar_direcao(labirinto[y][x], labirinto[y][x - 1], labirinto[y + 1][x], labirinto[y - 1][x - 1]):
-                y -= 1
-            elif direcao == "baixo" and y < altura - 2 and verificar_direcao(labirinto[y][x], labirinto[y][x + 1], labirinto[y + 1][x], labirinto[y - 1][x]):
-                y += 1
-            elif direcao == "esquerda" and x > 1 and verificar_direcao(labirinto[y][x - 1], labirinto[y][x + 1], labirinto[y + 1][x], labirinto[y - 1][x]):
-                x -= 1
-            elif direcao == "direita" and x < largura - 2 and verificar_direcao(labirinto[y][x + 1], labirinto[y][x - 1], labirinto[y + 1][x], labirinto[y - 1][x]):
+        i =0
+        while i <= 1:
+            direcao = self.gerar_direcao()#verificar se a direcao é valida. não é uma parede e nem ligada a dois caminhos
+            if i == 0:
                 x += 1
-            caminho[x][y] = "."  # Marcar o caminho
-        labirinto[y][x] = "S"
-                    
+                i+=1
+            elif direcao == "cima" and y > 1 and self.verificar_direcao(self.matriz[y][x], self.matriz[y][x - 1], self.matriz[y + 1][x], self.matriz[y - 1][x - 1]) and self.matriz[y][x] == " ":
+                y -= 1
+                i+=1
+            elif direcao == "baixo" and y < altura - 2 and self.verificar_direcao(self.matriz[y][x], self.matriz[y][x + 1], self.matriz[y + 1][x], self.matriz[y - 1][x]) and self.matriz[y][x] == " ":
+                y += 1
+                i+=1
+            elif direcao == "esquerda" and x >= 1 and self.verificar_direcao(self.matriz[y][x - 1], self.matriz[y][x + 1], self.matriz[y + 1][x], self.matriz[y - 1][x]) and self.matriz[y][x] == " ":
+                x -= 1
+                i+=1
+            elif direcao == "direita" and x <= largura - 2 and self.verificar_direcao(self.matriz[y][x + 1], self.matriz[y][x - 1], self.matriz[y + 1][x], self.matriz[y - 1][x]) and self.matriz[y][x] == " ":
+                x += 1
+                i+=1
+            self.matriz[y][x] = "."
+            
+        self.matriz[y][x] = "S"
+        return self.matriz            
 
 
-labirinto = Labirinto(21, 21)  
-print(labirinto)  
+labirinto = Labirinto(21, 21) 
+for i in range(0, labirinto.altura):
+    for j in range(0, labirinto.largura):
+        print(labirinto.matriz[i][j], end="")
+    print()  # Nova linha após cada linha do labirinto
 print("-----------------------------------------------------")
-labirinto = Labirinto.gerar_caminho_aleatorio(Labirinto.labirinto, Labirinto.largura, Labirinto.altura)
-print(labirinto)
+labirinto.matriz = labirinto.gerar_caminho_aleatorio( labirinto.largura, labirinto.altura)
+
+for i in range(0, labirinto.altura):
+    for j in range(0, labirinto.largura):
+        print(labirinto.matriz[i][j], end="")
+    print()  # Nova linha após cada linha do labirinto
